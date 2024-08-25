6367e4285da88681b84515120397360264d69dbf
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-08-25 21:52:06 +0200

    Implement `Missive` model extending `MessengerModel` with email and messaging capabilities.
    - Created `Missive` model extending the abstract `MessengerModel` for handling specific messaging tasks.
    - Added fields to store backend information, message ID, response, partner ID, error codes, and trace logs.
    - Implemented the `need_to_send` method to trigger sending based on the message status.
    - Included methods for clearing errors, marking messages as sent, and checking the status through backends.
    - Added a `url_viewer` property to generate URLs for viewing the content of missives.
    - Configured the model to be abstract and set custom permissions for checking missives.

---
+++ builder/applications/messenger/models/missive.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.utils.module_loading import import_string
+
+from builder.applications.messenger import choices, translates as _, send_missive
+from builder.applications.messenger.models.abstracts import MessengerModel
+
+class Missive(MessengerModel):
+    backend = models.CharField(max_length=255, editable=False)
+    subject = models.CharField(max_length=255)
+    msg_id = models.CharField(max_length=255, blank=True, null=True)
+
+    response = models.TextField(blank=True, null=True, editable=False)
+    partner_id = models.CharField(max_length=255, blank=True, null=True, editable=False)
+    code_error = models.CharField(max_length=255, blank=True, null=True, editable=False)
+    trace = models.TextField(blank=True, null=True, editable=False)
+    
+    class Meta(MessengerModel.Meta):
+        abstract = True
+        verbose_name = "missive"
+        verbose_name_plural = "missives"
+        permissions = [('can_check', _.permission_check),]
+        ordering = ['-date_create',]
+
+    def __str__(self):
+        return '%s (%s)' % (self.masking, self.subject)
+
+    def need_to_send(self):
+        if self.status == choices.STATUS_PREPARE and self.mode != choices.MODE_WEB:
+            send_missive(self)
+
+    def clear_errors(self):
+        self.trace = None
+        self.code_error = None
+
+    def to_sent(self):
+        self.clear_errors()
+        self.status = choices.STATUS_SENT
+
+    def get_backend(self):
+        backend = import_string(self.backend)()
+        return backend
+
+    def check_status(self):
+        backend = self.get_backend()
+        return getattr(backend, 'check_%s' % self.mode.lower())(self)
+
+    @property
+    def url_viewer(self):
+        from django.urls import reverse
+        return reverse('messenger-email-viewer', args=[self.uid])
