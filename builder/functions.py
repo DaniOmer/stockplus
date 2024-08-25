066075131123eda5fbb227b2cda8dd4f071b1e6e
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-08-25 19:37:05 +0200

    Implement a function to get messenger backend based on MissiveBackend define in settings file.

--- builder/functions.py
+++ builder/functions.py
@@ -1,4 +1,16 @@
 from django.conf import settings
+from django.utils.module_loading import import_string
+from django.core.exceptions import ImproperlyConfigured
 
 def setting(name, default=None):
     return getattr(settings, name, default)
+
+def get_backends(backends_list, return_tuples=False, path_extend='', *args, **kwargs):
+    backends = []
+    for backend_path in backends_list:
+        backend_path = backend_path + path_extend
+        backend = import_string(backend_path)(*args, **kwargs)
+        backends.append((backend, backend_path) if return_tuples else backend)
+    if not backends:
+        raise ImproperlyConfigured('No backends have been defined.')
+    return backends
--- configuration/settings.py
+++ configuration/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_ckeditor_5',
     'builder',
     'builder.applications.messenger',
 ]
