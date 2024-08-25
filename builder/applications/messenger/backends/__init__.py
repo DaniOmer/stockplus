9d7f4d027d194380e036ed6683b8d55dbe0e8761
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-08-25 18:47:15 +0200

    feat(messenger): implement MissiveBackend class for handling email and SMS sending
    - Add MissiveBackend class to manage email and SMS sending
    - Implement methods for sending emails with attachments and handling SMS logic
    - Configure logging and support for service-based email sending

---
+++ builder/applications/messenger/backends/__init__.py
@@ -0,0 +1,89 @@
+from django.core.mail import EmailMultiAlternatives
+from django.core.mail.message import make_msgid
+
+from builder.functions import setting
+from builder.applications.messenger import choices
+from builder.applications.messenger.apps import MessengerConfig as conf
+import logging, os
+
+logger = logging.getLogger(__name__)
+
+class MissiveBackend():
+    email = None
+    sms = None
+
+    def __init__(self, missive, *args, **kwargs):
+        self.missive = missive
+
+    @property
+    def extra(self):
+        return {'user': self.missive.content_object, 'app': 'messenger'}
+
+    @property
+    def message(self):
+        return self.missive.txt if self.missive.txt else self.missive.html
+
+    def send(self):
+        return getattr(self, 'send_%s' % self.missive.mode.lower())()
+
+    def send_sms(self):
+        over_target = setting('MISSIVE_PHONE', False)
+        self.missive.target = over_target if over_target else self.missive.target
+
+        if setting('MISSIVE_SERVICE', False):
+            pass
+
+        self.missive.status = choices.STATUS_SENT
+        self.missive.save()
+
+        logger.info("send sms: %s" % self.message, extra=self.extra)
+
+        return self.missive.status
+
+    def email_attachments(self):
+        if self.missive.attachments:
+            logs = []
+            for document in self.missive.attachments:
+                if setting('MISSIVE_SERVICE', False):
+                    self.email.attach(os.path.basename(document.name), document.read(), 'application/pdf')
+                logs.append(os.path.basename(document.name))
+            self.missive.logs['attachments'] = logs
+
+        if setting('MISSIVE_SERVICE', False):
+            self.email.send()
+
+    @property
+    def sender_email(self):
+        if self.missive.name:
+            return "%s <%s>" % (self.missive.name, self.missive.sender)
+        return self.missive.sender
+
+    @property
+    def reply_email(self):
+        return [self.missive.reply] if self.missive.reply else [self.missive.sender]
+
+    def send_email(self):
+        over_target = setting('MISSIVE_EMAIL', False)
+        self.missive.target = over_target if over_target else self.missive.target
+
+        if setting('MISSIVE_SERVICE', False):
+            self.missive.msg_id = make_msgid()
+            text_content = str(self.missive.txt)
+            html_content = self.missive.html_format
+            self.email = EmailMultiAlternatives(
+                self.missive.subject,
+                html_content,
+                self.sender_email,
+                [self.missive.target],
+                reply_to=self.reply_email,
+                headers={'Message-Id': self.missive.msg_id}
+            )
+            self.email.attach_alternative(html_content, "text/html")
+
+        self.email_attachments()
+        self.missive.to_sent()
+        self.missive.save()
+        return self.missive.status
+
+    def check(self, missive):
+        return True
--- configuration/settings.py
+++ configuration/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'builder',
+    'builder.applications.messenger',
 ]
 
 MIDDLEWARE = [
