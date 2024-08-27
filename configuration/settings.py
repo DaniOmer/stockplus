1914cae212499bc533eec4493eca526480f7c39d
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-08-27 15:12:11 +0200

    Add rest_framework as an application and register stockplus urls inside config urls file.

--- configuration/settings.py
+++ configuration/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'django_ckeditor_5',
     'builder',
     'builder.applications.messenger',
