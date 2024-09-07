b943d29bce77135d678fb63609f8e62a07f7c84f
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-09-07 02:12:09 +0200

    Implement the logic to retrieve, update and delete a point of sale.

--- stockplus/applications/pointofsale/urls.py
+++ stockplus/applications/pointofsale/urls.py
@@ -4,6 +4,7 @@ from stockplus.applications.pointofsale import views
 
 urlpatterns = [
     path('api/', include([
-        path('point-of-sale/', views.PointOfSaleListCreateView.as_view(), name='point_of_sale_list_create')
+        path('point-of-sale/', views.PointOfSaleListCreateView.as_view(), name='point_of_sale_list_create'),
+        path('point-of-sale/<int:pk>', views.PointOfSaleRetrievUpdateDeleteView.as_view(), name='point_of_sale_list_create')
     ]))
 ]
--- stockplus/applications/pointofsale/views/__init__.py
+++ stockplus/applications/pointofsale/views/__init__.py
@@ -1,5 +1,7 @@
-from stockplus.applications.pointofsale.views.create import PointOfSaleListCreateView
+from stockplus.applications.pointofsale.views.list_create import PointOfSaleListCreateView
+from stockplus.applications.pointofsale.views.retrieve_update_delete import PointOfSaleRetrievUpdateDeleteView
 
 __all__ = (
     PointOfSaleListCreateView,
+    PointOfSaleRetrievUpdateDeleteView,
 )
