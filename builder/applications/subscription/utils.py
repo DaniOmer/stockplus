e78189346857dcafdbf51cf3c02ae7c271d84d23
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-09-17 15:12:53 +0200

    fix: correct group assignment logic in add_users_to_subscription_group
    - Fixed incorrect handling of subscription group IDs by excluding active subscription groups properly.
    - Replaced bitwise OR operation with set.add() for group assignment.
    - Improved logic for handling company users and individual user group assignments.
    - Ensured users are only added to relevant subscription groups and removed from others.

--- builder/applications/subscription/utils.py
+++ builder/applications/subscription/utils.py
@@ -1,13 +1,35 @@
-from builder.models import User
+from builder.models import User, Subscription
 
 def add_users_to_subscription_group(subscription):
-    """Add a user to a subscription group"""
+    """Add users to a subscription group."""
+    subscription_plan_obj = subscription.subscription_plan
+    # Filter active subscriptions, excluding the current one
+    subs_qs = Subscription.objects.filter(active=True).exclude(id=subscription.id)
+    subs_groups = subs_qs.values_list('subscription_plan__group_id', flat=True)
+    subs_groups_set = set(subs_groups)
+
+    # Group associated to the current subscription
+    group = subscription_plan_obj.group
+    group_id = group.id
+
     if subscription.company:
         users = User.objects.filter(company=subscription.company)
         for user in users:
-            user.groups.add(subscription.subscription_plan.group)
+            current_user_groups = set(user.groups.all().values_list('id', flat=True))
+            # Subtract the groups from active subscriptions
+            current_user_groups_set = current_user_groups - subs_groups_set
+            # Add the current subscription group
+            current_user_groups_set.add(group_id)
+            user.groups.set(list(current_user_groups_set))
     else:
-        subscription.user.groups.add(subscription.subscription_plan.group)
+        user = subscription.user
+        current_user_groups = set(user.groups.all().values_list('id', flat=True))
+        # Subtract the groups from active subscriptions
+        current_user_groups_set = current_user_groups - subs_groups_set
+        # Add the current subscription group
+        current_user_groups_set.add(group_id)
+        user.groups.set(list(current_user_groups_set))
+
 
 def remove_users_from_subscription_group(subscription):
     """Removes users from the subscription group"""
