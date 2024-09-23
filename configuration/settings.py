6199845ed7ea7cce1f1d643b8cd8ae0cc65601ba
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-09-23 12:17:59 +0200

    Update settings file to allow specified host.

--- configuration/settings.py
+++ configuration/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=True, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split()
 
 ENV = config('ENV', default="DEVELOPMENT")
 
