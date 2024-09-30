7a9ff4608273b01c8e58d4b9614fbd8e3f677293
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-09-30 13:28:11 +0200

    Implement ProductService for managing actions related to Stripe product and add a new method create_stripe_product for creating stripe product.

--- builder/applications/shop/services/__init__.py
+++ builder/applications/shop/services/__init__.py
@@ -1,5 +1,7 @@
 from builder.applications.shop.services.customer import CustomerService
+from builder.applications.shop.services.product import ProductService
 
 __all__ = (
     CustomerService,
+    ProductService,
 )
