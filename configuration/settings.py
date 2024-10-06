536b3984a63197acf599ca066e00f949ebb1d19f
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-10-06 21:58:43 +0200

    Use django utils to generate random secret key to use as default by docker.

--- configuration/settings.py
+++ configuration/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
