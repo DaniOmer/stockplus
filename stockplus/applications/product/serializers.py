9a42d139866aa15fc3a7f6f6ba68598a30ab18ce
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-10-15 21:55:08 +0200

    Implement serializer related to product model.

---
+++ stockplus/applications/product/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from stockplus.models import (
+    Brand, Product, ProductCategory,
+    ProductFeature, ProductVariant, PointOfSaleProductVariant
+)
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['name', 'description', 'logo_url']
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ['name', 'description']
+
+class ProductFeatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductFeature
+        fields = ['name', 'description']
+
+class PointOfSaleProductVariantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PointOfSaleProductVariant
+        fields = ['id', 'point_of_sale', 'stock']
+
+class ProductVariantSerializer(serializers.ModelSerializer):
+    stocks = PointOfSaleProductVariantSerializer(many=True)
+    class Meta:
+        model = ProductVariant
+        fields = ['id', 'product', 'color', 'size', 'price', 'buy_price', 'sku', 'stocks']
+
+class ProductSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer(many=False, required=False)
+    category = ProductCategorySerializer(many=False, required=True)
+    variants = ProductVariantSerializer(many=True)
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'brand', 'category', 'variants']
+    
+    def create(self, validated_data):
+        request = self.context.get('request')
+        company = request.user.company
+
+        if not company:
+            raise ValidationError('You must provide your company information to continue.')
+        
+        variants_data = validated_data.pop('variants', [])
+        product = Product.objects.create(company=company, **validated_data)
+        for variant_data in variants_data:
+            product_variant = ProductVariantSerializer.objects.create(product=product, **variant_data)
+            for stock_data in variant_data['stocks']:
+                PointOfSaleProductVariantSerializer.objects.create(product_variant=product_variant, **stock_data)
+        return product
